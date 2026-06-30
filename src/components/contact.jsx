72f88d2822d76991d132936d8ad3d00b2a11f7ed
Rajat Sharma <rajatsharmaw90gmail.com>
2026-06-30 12:37:05 -0700

    api changed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,7 +20,7 @@ export default function Contact(){
 
     try {
       const response = await fetch(
-        "http://gardening.inorbitservices.ca/gardening/contact",
+        "http://api.inorbitservices.ca/gardening/contact",
         {
           method: "POST",
           headers: {
